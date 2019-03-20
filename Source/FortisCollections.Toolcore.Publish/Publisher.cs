349c55204407111306af91c675be469ed9184552
klpatil <kpatil@horizontalintegration.com>
2019-03-20 14:22:42 +0530

    - Toolcore is updated to support Sitecore's publishing service - PublishManager's PublishItem API method is used (Thanks to #publishservice channel on slack) - Also, Was getting error after getting latest related to csc.exe -- Committed them as well. - There are few things whihc can be imporved e.g. Sleep time can be configurable etc.

--- Source/FortisCollections.Toolcore.Publish/Publisher.cs
+++ Source/FortisCollections.Toolcore.Publish/Publisher.cs
@@ -1,78 +1,100 @@
-﻿using Sitecore.Configuration;
+﻿using Sitecore;
+using Sitecore.Configuration;
 using Sitecore.Data;
 using Sitecore.Data.Managers;
+using Sitecore.Jobs;
 using Sitecore.Publishing;
 using Sitecore.SecurityModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 namespace FortisCollections.Toolcore.Publish
 {
-	public class Publisher : IPublisher
-	{
-		public string Publish(IPublisherOptions options)
-		{
-			using (new SecurityDisabler())
-			{
-				var publisherOptions = SetDefaults(options);
-				var sourceDatabase = Factory.GetDatabase(publisherOptions.SourceDatabaseName);
-				var languages = LanguageManager.GetLanguages(sourceDatabase).AsEnumerable();
+    public class Publisher : IPublisher
+    {
+        public string Publish(IPublisherOptions options)
+        {
+            using (new SecurityDisabler())
+            {
+                var publisherOptions = SetDefaults(options);
+                var sourceDatabase = Factory.GetDatabase(publisherOptions.SourceDatabaseName);
+                var languages = LanguageManager.GetLanguages(sourceDatabase).AsEnumerable();
 
-				if (publisherOptions.LanguageNames != null && publisherOptions.LanguageNames.Length > 0)
-				{
-					languages = languages.Where(l => publisherOptions.LanguageNames.Contains(l.Name));
-				}
+                if (publisherOptions.LanguageNames != null && publisherOptions.LanguageNames.Length > 0)
+                {
+                    languages = languages.Where(l => publisherOptions.LanguageNames.Contains(l.Name));
+                }
 
-				var targets = PublishManager.GetPublishingTargets(sourceDatabase).AsEnumerable();
-				var targetDatabases = new List<Database>();
+                var targets = PublishManager.GetPublishingTargets(sourceDatabase).AsEnumerable();
+                var targetDatabases = new List<Database>();
 
-				if (publisherOptions.TargetNames != null && publisherOptions.TargetNames.Length > 0)
-				{
-					targets = targets.Where(t => publisherOptions.TargetNames.Contains(t.Name));
-				}
+                if (publisherOptions.TargetNames != null && publisherOptions.TargetNames.Length > 0)
+                {
+                    targets = targets.Where(t => publisherOptions.TargetNames.Contains(t.Name));
+                }
 
-				foreach (var target in targets)
-				{
-					targetDatabases.Add(Factory.GetDatabase(target["Target database"]));
-				}
+                foreach (var target in targets)
+                {
+                    targetDatabases.Add(Factory.GetDatabase(target["Target database"]));
+                }
 
-				var publishMode = ParsePublishMode(publisherOptions.PublishMode);
+                var publishMode = ParsePublishMode(publisherOptions.PublishMode);
 
-				var publishOptions = new PublishOptions(
-					sourceDatabase,
-					targetDatabases.First(),
-					publishMode,
-					languages.First(),
-					DateTime.Now,
-					publisherOptions.TargetNames.ToList())
-				{
-					Deep = publisherOptions.Deep,
-					RootItem = sourceDatabase.Items[publisherOptions.RootItem]
-				};
+                var publishOptions = new PublishOptions(
+                    sourceDatabase,
+                    targetDatabases.First(),
+                    publishMode,
+                    languages.First(),
+                    DateTime.Now,
+                    publisherOptions.TargetNames.ToList())
+                {
+                    Deep = publisherOptions.Deep,
+                    RootItem = sourceDatabase.Items[publisherOptions.RootItem]
+                };
 
-				var publisher = new Sitecore.Publishing.Publisher(publishOptions, languages);
-				var job = publisher.PublishAsync();
+                Handle jobHandle = null;
+                jobHandle = Sitecore.Publishing.PublishManager.PublishItem(
+                    sourceDatabase.Items[publisherOptions.RootItem], targetDatabases.ToArray(),
+                    languages.ToArray(), publisherOptions.Deep, true, false);
+                // We need to keep a wait
+                // as it takes sometime to
+                // Trigger job
+                // Try for 10 seconds
+                // TODO :  Make this configurable
+                Job job = null;
+                for (int i = 0; i <= 10; i++)
+                {
+                    job = Sitecore.Jobs.JobManager.GetJob(jobHandle);
+                    if (job != null)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Thread.Sleep(1000);
+                    }
+                }
+                return job.Name;
+            }
+        }
 
-				return job.Name;
-			}
-		}
+        public IPublisherOptions SetDefaults(IPublisherOptions publisherOptions)
+        {
+            return (new DefaultPublisherOptionsFactory()).Create(publisherOptions);
+        }
 
-		public IPublisherOptions SetDefaults(IPublisherOptions publisherOptions)
-		{
-			return (new DefaultPublisherOptionsFactory()).Create(publisherOptions);
-		}
+        public PublishMode ParsePublishMode(string unparsedPublishMode)
+        {
+            PublishMode publishMode;
 
-		public PublishMode ParsePublishMode(string unparsedPublishMode)
-		{
-			PublishMode publishMode;
+            if (!Enum.TryParse(unparsedPublishMode, out publishMode))
+            {
+                publishMode = PublishMode.Smart;
+            }
 
-			if (!Enum.TryParse(unparsedPublishMode, out publishMode))
-			{
-				publishMode = PublishMode.Smart;
-			}
-
-			return publishMode;
-		}
-	}
+            return publishMode;
+        }
+    }
 }
